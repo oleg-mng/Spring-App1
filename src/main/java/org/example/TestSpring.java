9fdf610a2efb17c1eb83d556f3fbf542ff30e7ea
oleg <legik7127@gmail.com>
2023-05-13 11:57:30 +0600

    added applicationContext.xml

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -5,7 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
-        );
+                "applicationContext.xml");
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+        System.out.println(testBean.getName());
+        context.close();
     }
 }
