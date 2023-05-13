4bc13849035a4dab2e7530eb6cc027530ea13c80
oleg <legik7127@gmail.com>
2023-05-13 15:17:51 +0600

    realized MusicPlayer musicPlayer

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -7,4 +7,7 @@ public class MusicPlayer {
     public MusicPlayer(Music music){
         this.music = music;
     }
+    public void playMusic(){
+        System.out.println("Playing: "+ music.getSong());
+    }
 }
