7d4be011c50f73696a49dc85ad84766df93cfc7a
oleg <legik7127@gmail.com>
2023-05-15 00:04:00 +0600

    working with views scope

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -11,19 +11,29 @@ public class TestSpring {
                 "applicationContext.xml");
 //        Music music = context.getBean("musicBean", Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean right = firstMusicPlayer == secondMusicPlayer;
+        System.out.println(right);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(7);
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
 //        musicPlayer.playMusic();
 
-        RapMusic rapMusic = context.getBean("musicBeanRap", RapMusic.class);
-        RockMusic rockMusic = context.getBean("musicBeanRock", RockMusic.class);
-        ClassicalMusic classicalMusic = context.getBean("musicBeanClassical", ClassicalMusic.class);
-
-        List<Music> list = new ArrayList<>();
-        list.add(rapMusic);
-        list.add(rockMusic);
-        list.add(classicalMusic);
-        musicPlayer.setMusicList(list);
-        musicPlayer.playMusic(list);
+//        RapMusic rapMusic = context.getBean("musicBeanRap", RapMusic.class);
+//        RockMusic rockMusic = context.getBean("musicBeanRock", RockMusic.class);
+//        ClassicalMusic classicalMusic = context.getBean("musicBeanClassical", ClassicalMusic.class);
+//
+//        List<Music> list = new ArrayList<>();
+//        list.add(rapMusic);
+//        list.add(rockMusic);
+//        list.add(classicalMusic);
+//        musicPlayer.setMusicList(list);
+//        musicPlayer.playMusic(list);
 //
 //        System.out.println(musicPlayer.getName());
 //        System.out.println(musicPlayer.getVolume());
