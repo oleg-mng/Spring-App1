cdf49d78024e47d30cc451f34ec07f59bfd3dd48
oleg <legik7127@gmail.com>
2023-05-15 22:25:00 +0600

    added methods doMyInit() and doMyDestroy()

--- src/main/java/org/example/RapMusic.java
+++ src/main/java/org/example/RapMusic.java
@@ -1,6 +1,12 @@
 package org.example;
 
 public class RapMusic implements Music{
+    public void doMyInit(){
+        System.out.println("do my initialization");
+    }
+    public void doMyDestroy(){
+        System.out.println("do my destruction");
+    }
     @Override
     public String getSong() {
         return "California dreaming";
